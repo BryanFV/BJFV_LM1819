fe9a9ae4a9c3e7b0bada635142b48f7eeecd410a
Alumno <brayan.j.fernandez.v@gmail.com>
2019-02-14 10:14:14 +0100

    formularios con condicionales

--- PHP/Ejercicio06/resultado.php
+++ PHP/Ejercicio06/resultado.php
@@ -1,5 +1,12 @@
 <?php
+if ($_REQUEST["nombre"] !=""){
+    print "<p>Su nombre es $_REQUEST[nombre]</p>";
+}
+if (isset($_REQUEST["opcion"])){
+    print "<p>Opción Marcada</p>";
+}
 print_r($_REQUEST);
+print_r($_FILES);
 
 
 ?>

af53c2849f3bd710ea5dc27ccc4dd5d3a4fbdc63
Alumno <brayan.j.fernandez.v@gmail.com>
2019-03-05 13:41:19 +0100

    GBD PINACO

--- GBD/pinacotecas.sql
+++ GBD/pinacotecas.sql
@@ -8,7 +8,10 @@ select id_pintor from pintores;
 select id_mecenas from mecenas;
 select id_escuela from escuelas;
 --3.Usando concatenación de textos, se pide listar los datos de la tabla pintores con las siguientes columnas: "IDENTIFICADOR", "NOMBRE", "ID-NOMBRE", donde ID-NOMBRE será el resultado de la concatenación del campo ID_PINTOR y de N_PINTOR.
+SELECT PINTORES.id_pintor, PINTORES.N_PINTOR AS "ID-PINTOR" FROM PINTORES;
+SELECT concat(id_pintor, N_PINTOR) AS "ID-PINTOR" FROM PINTORES;
 --4.Usando concatenación de textos, listar el contenido de la tabla pinacotecas de la siguiente forma: "Pinacoteca (ID_PINACOTECA): (NOMBRE)".
+ ASDAS
 --5.Seleccionar nombre y país de la tabla pinacotecas donde ID_PINACOTECA es "MUBAS".
 --6.Seleccionar técnica, ancho y alto de los siguientes cuadros: La Gioconda, El nacimiento de Venus y Sagrada Familia.
 --7.Seleccionar nombre y ciudad de nacimiento de los mecenas italianos.

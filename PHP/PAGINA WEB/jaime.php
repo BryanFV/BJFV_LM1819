7fc0991e852f8edfd717a88caf1c6c50ddb79bf8
Alumno <brayan.j.fernandez.v@gmail.com>
2019-03-29 14:12:45 +0100

    actualizacion y avance del proyecto

--- PHP/PAGINA WEB/jaime.php
+++ PHP/PAGINA WEB/jaime.php
@@ -9,6 +9,9 @@
 
 </head>
 <body>
+<div class="border border-primary">
+<h1 class="display-2">Los datos</h1>
+
 <form>
   <div class="form-row">
     <div class="form-group col-md-6">
@@ -23,19 +26,39 @@
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="tipo">Tipo de Medida</label>
-      <input type="date" class="form-control" id="tipo" placeholder="Tipo...">
+      <input type="text" class="form-control" id="tipo" placeholder="Tipo...">
     </div>
     <div class="form-group col-md-6">
       <label for="valor">Valor Medida</label>
-      <input type="date" class="form-control" id="valor" placeholder="Valor...">
+      <input type="text" class="form-control" id="valor" placeholder="Valor...">
     </div>
   </div>
   <button type="submit" class="btn btn-outline-success btn-lg btn-block">Buscar</button>
 </form>
+<h1 class="display-4">La tabla</h1>
+<table class="table table-sm table-dark">
+  <thead>
+    <tr >
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+  <tr>
+  <td class="bg-primary">...</td>
+  <td class="bg-success">...</td>
+  <td class="bg-warning">...</td>
+  <td class="bg-danger">...</td>
+  
+</tr>
+  </tbody>
+</table>
 
 
 
-
+<div>
     <!--JavaBooststrap-->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>

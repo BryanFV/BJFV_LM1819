9f1c852fba780d08c10079ebe921033692bc55b4
Alumno <brayan.j.fernandez.v@gmail.com>
2019-05-14 12:55:12 +0200

    Cursores

---
+++ GBD/PL/SQL/cursor.sql
@@ -0,0 +1,14 @@
+SET SERVEROUTPUT ON
+DECLARE
+CURSOR emp_cursor IS
+ SELECT first_name, email FROM employees;
+ v_reg_cursor emp_cursor%ROWTYPE;
+BEGIN
+OPEN emp_cursor;
+FETCH emp_cursor INTO v_reg_cursor;
+    WHILE emp_cursor%FOUND LOOP
+    DBMS_OUTPUT.PUT_LINE(v_reg_cursor.first_name||'*'|| v_reg_cursor.email);
+    FETCH emp_cursor INTO v_reg_cursor;
+END LOOP;
+CLOSE emp_cursor;
+END;

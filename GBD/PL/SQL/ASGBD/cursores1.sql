ed76b471d01eb7a5b16b2eacd68f1d0eb5b127de
Fernßndez Valencia <bferval721@IESCJ.local>
2019-11-18 09:32:26 +0100

    COMPLETANDO EJERCICIOS

--- GBD/PL/SQL/ASGBD/cursores1.sql
+++ GBD/PL/SQL/ASGBD/cursores1.sql
@@ -1,6 +1,24 @@
 --Ejercicios iniciales de cursores:
 
 --1. Desarrollar un procedimiento que visualice el nombre y apellido de todos los empleados ordenados por número de departamento. --> 1 punto
+SET SERVEROUTPUT ON
+CREATE OR REPLACE PROCEDURE P_NOMBRE_D
+IS
+CURSOR C_NOMBRE IS 
+ SELECT FIRST_NAME, LAST_NAME, DEPARTMENT_ID FROM employees order by department_id;
+ V_NOMBRE C_NOMBRE%ROWTYPE;
+BEGIN
+OPEN C_NOMBRE;
+FETCH C_NOMBRE INTO V_NOMBRE;
+    WHILE C_NOMBRE%FOUND LOOP
+    DBMS_OUTPUT.PUT_LINE('Nombre: '||V_NOMBRE.FIRST_NAME||' Apellido: '|| V_NOMBRE.LAST_NAME ||' DEPARTAMENTO: '||V_NOMBRE.DEPARTMENT_ID);
+    FETCH C_NOMBRE INTO V_NOMBRE;
+END LOOP;
+CLOSE C_NOMBRE;
+END;
+/
+EXEC P_NOMBRE_D();
+
 --3. Realizar un procedimiento que visualice el número y nombre de un empleado, así como el nombre de su departamento, ordenados por nombre de departamento. --> 1 punto
 SET SERVEROUTPUT ON
 CREATE OR REPLACE PROCEDURE p_empleado
@@ -22,7 +40,23 @@ END;
 EXEC p_empleado()
 
 --2. Desarrollar un procedimiento que encuentre el primer empleado con un sueldo menor de 6.000 €. --> 0.5 puntos
-
+SET SERVEROUTPUT ON
+CREATE OR REPLACE PROCEDURE P_SUELDO
+IS
+CURSOR C_SUELDO IS
+    SELECT FIRST_NAME, SALARY FROM EMPLOYEES WHERE SALARY < 6000 ORDER BY asc;
+    V_SUELDO C_SUELDO%ROWTYPE;
+BEGIN
+    OPEN C_SUELDO;
+        FETCH C_SUELDO INTO V_SUELDO;
+            WHILE C_SUELDO%FOUND LOOP
+            DBMS_OUTPUT.PUT_LINE('EL EMPLEADO CON EL SUELDO INFERIOR A 6000 € ES: '||V_SUELDO.FIRST_NAME || 'SUELDO: '||v_sueldo.SALARY);
+            FETCH C_SUELDO INTO V_SUELDO;
+        END LOOP;
+    CLOSE C_SUELDO;
+END;
+/
+exec P_SUELDO();
 --3. Realizar un procedimiento que visualice el número y nombre de un empleado, así como el nombre de su departamento, ordenados por nombre de departamento. --> 1 punto
 SET SERVEROUTPUT ON
 CREATE OR REPLACE PROCEDURE p_empleado
@@ -43,7 +77,23 @@ END;
 /
 EXEC p_empleado()
 --4. Escribir un procedimiento que reciba una cadena y visualice el nombre y el número de empleado de todos los empleados cuyo nombre contenga la cadena especificada. Al finalizar visualizar el número de empleados mostrados. --> 1.5 punto
-
+SET SERVEROUTPUT ON
+CREATE OR REPLACE PROCEDURE CONCATENA (v_cadena in VARCHAR2)
+DECLARE
+CURSOR buscar IS
+ SELECT first_name, employee_id FROM employees where first_name like v_escribe;
+ v_escribe buscar%ROWTYPE;
+BEGIN
+    v_escribe VARCHAR2(50):='%'||&m_cadena||'%';
+OPEN buscar;
+FETCH buscar INTO v_escribe;
+    WHILE buscar%FOUND LOOP
+    DBMS_OUTPUT.PUT_LINE('Nombre: '||v_escribe.first_name||' Número: '||v_escribe.employee_id);
+    FETCH buscar INTO v_escribe;
+END LOOP;
+dbms_output.put_line('Hay '||buscar%ROWCOUNT||' empleados.');
+CLOSE buscar;
+END;
 --5. Crear un procedimiento que muestre las localizaciones (dirección y ciudad) y el número de departamentos que tiene (incluso si no tiene departamentos). --> 2 puntos
 
 --6. Escribir un programa que visualice el nombre, apellido y el salario de los cuatro empleados que tienen el salario más bajo. --> 2 puntos
